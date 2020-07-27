47d84d4fd9b3a5daa0ee23b763c096414970ec27
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-07-27 01:10:50 -0400

    More comments in assembly listings
    - Commented BONUS.ASM

--- src/CoCo/asm.bat
+++ src/CoCo/asm.bat
@@ -1,5 +1,5 @@
 lwasm -9 -b --list=xplode.lst --symbols -o xplode.bin XPLODE.ASM
-lwasm -9 -b --list=bonus.lst --symbols -o bonus.bin BONUS.ASM
+rem lwasm -9 -b --list=bonus.lst --symbols -o bonus.bin BONUS.ASM
 rem lwasm -9 -b --list=putspr.lst --symbols -o putspr.bin PUTSPR.ASM
 lwasm -9 -b --list=lindrw.lst --symbols -o lindrw.bin LINDRW.ASM
 lwasm -9 -b --list=data3.lst --symbols -o data3.bin DATA3.ASM
--- src/cross-asm/asm.bat
+++ src/cross-asm/asm.bat
@@ -1,5 +1,5 @@
 rem lwasm -9 -b --list=xplode.lst --symbols -o xplode.bin XPLODE.ASM
-rem lwasm -9 -b --list=bonus.lst --symbols -o bonus.bin BONUS.ASM
+lwasm -9 -b --list=bonus.lst --symbols -o bonus.bin BONUS.ASM
 lwasm -9 -b --list=putspr.lst --symbols -o putspr.bin PUTSPR.ASM
 rem lwasm -9 -b --list=lindrw.lst --symbols -o lindrw.bin LINDRW.ASM
 rem lwasm -9 -b --list=data3.lst --symbols -o data3.bin DATA3.ASM
