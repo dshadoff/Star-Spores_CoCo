8249ade6471f46216f721d45af9fe576eed77caa
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-07-26 22:59:12 -0400

    Begin commenting source files
    - migrate the recompilation from src/CoCo directory to src/cross-asm
    - begin by commneting PUTSPR.ASM
    - remove line numbering as it is unnecessary for lwasm, and affects formatting

--- src/CoCo/asm.bat
+++ src/CoCo/asm.bat
@@ -1,6 +1,6 @@
 lwasm -9 -b --list=xplode.lst --symbols -o xplode.bin XPLODE.ASM
 lwasm -9 -b --list=bonus.lst --symbols -o bonus.bin BONUS.ASM
-lwasm -9 -b --list=putspr.lst --symbols -o putspr.bin PUTSPR.ASM
+rem lwasm -9 -b --list=putspr.lst --symbols -o putspr.bin PUTSPR.ASM
 lwasm -9 -b --list=lindrw.lst --symbols -o lindrw.bin LINDRW.ASM
 lwasm -9 -b --list=data3.lst --symbols -o data3.bin DATA3.ASM
 lwasm -9 -b --list=sound.lst --symbols -o sound.bin SOUND.ASM
