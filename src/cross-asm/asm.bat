0e4a720ce7132d33e882f9fd5d72abb75aad32ef
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-07-30 10:03:10 -0400

    More commented source

--- src/cross-asm/asm.bat
+++ src/cross-asm/asm.bat
@@ -5,5 +5,5 @@ rem lwasm -9 -b --list=lindrw.lst --symbols -o lindrw.bin LINDRW.ASM
 lwasm -9 -b --list=data3.lst --symbols -o data3.bin DATA3.ASM
 lwasm -9 -b --list=sound.lst --symbols -o sound.bin SOUND.ASM
 rem lwasm -9 -b --list=noise.lst --symbols -o noise.bin NOISE.ASM
-rem lwasm -9 -b --list=setpt.lst --symbols -o setpt.bin SETPT.ASM
+lwasm -9 -b --list=setpt.lst --symbols -o setpt.bin SETPT.ASM
 
