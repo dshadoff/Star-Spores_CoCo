67d884ecd86c22c66b68de1b90558b2c909e60d0
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-08-10 22:46:35 -0400

    More comments

--- src/CoCo/asm.bat
+++ src/CoCo/asm.bat
@@ -4,6 +4,6 @@ rem lwasm -9 -b --list=putspr.lst --symbols -o putspr.bin PUTSPR.ASM
 lwasm -9 -b --list=lindrw.lst --symbols -o lindrw.bin LINDRW.ASM
 rem lwasm -9 -b --list=data3.lst --symbols -o data3.bin DATA3.ASM
 rem lwasm -9 -b --list=sound.lst --symbols -o sound.bin SOUND.ASM
-lwasm -9 -b --list=noise.lst --symbols -o noise.bin NOISE.ASM
+rem lwasm -9 -b --list=noise.lst --symbols -o noise.bin NOISE.ASM
 rem lwasm -9 -b --list=setpt.lst --symbols -o setpt.bin SETPT.ASM
 
--- src/cross-asm/asm.bat
+++ src/cross-asm/asm.bat
@@ -4,6 +4,6 @@ lwasm -9 -b --list=putspr.lst --symbols -o putspr.bin PUTSPR.ASM
 rem lwasm -9 -b --list=lindrw.lst --symbols -o lindrw.bin LINDRW.ASM
 lwasm -9 -b --list=data3.lst --symbols -o data3.bin DATA3.ASM
 lwasm -9 -b --list=sound.lst --symbols -o sound.bin SOUND.ASM
-rem lwasm -9 -b --list=noise.lst --symbols -o noise.bin NOISE.ASM
+lwasm -9 -b --list=noise.lst --symbols -o noise.bin NOISE.ASM
 lwasm -9 -b --list=setpt.lst --symbols -o setpt.bin SETPT.ASM
 
