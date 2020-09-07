45a6369317379be0a667ecda8a6040c6e6503fd7
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-09-06 23:37:29 -0400

    Add DATA4B, DATA4C

--- src/CoCo/asm.bat
+++ src/CoCo/asm.bat
@@ -6,8 +6,10 @@ rem lwasm -9 -b --list=data1.lst --symbols -o data1.bin DATA1.ASM
 rem lwasm -9 -b --list=data2.lst --symbols -o data1.bin DATA2.ASM
 rem lwasm -9 -b --list=data3.lst --symbols -o data3.bin DATA3.ASM
 rem lwasm -9 -b --list=data4a.lst --symbols -o data4a.bin DATA4A.ASM
+rem lwasm -9 -b --list=data4b.lst --symbols -o data4b.bin DATA4B.ASM
+rem lwasm -9 -b --list=data4c.lst --symbols -o data4c.bin DATA4C.ASM
 rem lwasm -9 -b --list=sound.lst --symbols -o sound.bin SOUND.ASM
 rem lwasm -9 -b --list=noise.lst --symbols -o noise.bin NOISE.ASM
 rem lwasm -9 -b --list=setpt.lst --symbols -o setpt.bin SETPT.ASM
-rem lwasm -9 -b --list=main.lst --symbols -o main.bin main.ASM
+rem lwasm -9 -b --list=main.lst --symbols -o main.bin MAIN.ASM
 
